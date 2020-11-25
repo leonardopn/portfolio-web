356b2d13989b2df8efa3e57a0a06a5dd28b430fb
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2020-11-25 20:09:13 -0300

    Updates

---
+++ precache-manifest.eab12675a348ed93e14c2441358f6681.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e05e9ab186c360e05fc8148b242371",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "7345d60334c645b1ea82",
+    "url": "/portfolio-web/static/css/main.fd6c1063.chunk.css"
+  },
+  {
+    "revision": "aad37fe63e41fec77eb0",
+    "url": "/portfolio-web/static/js/2.a33e9495.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-web/static/js/2.a33e9495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7345d60334c645b1ea82",
+    "url": "/portfolio-web/static/js/main.53824571.chunk.js"
+  },
+  {
+    "revision": "02c70c98d84771f7c55b",
+    "url": "/portfolio-web/static/js/runtime-main.a46eacad.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/portfolio-web/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "d251c62495ce22b0543248ee0e7fec41",
+    "url": "/portfolio-web/static/media/icon-unip.d251c624.png"
+  },
+  {
+    "revision": "4297abcb83096d76b549a83164e4337f",
+    "url": "/portfolio-web/static/media/icons8-javascript-100.4297abcb.png"
+  },
+  {
+    "revision": "8e32d5a869fdb02d70fab3b27550841e",
+    "url": "/portfolio-web/static/media/profile.8e32d5a8.png"
+  },
+  {
+    "revision": "f3bd43960fe58efe8e2b3332151dd615",
+    "url": "/portfolio-web/static/media/profile2.f3bd4396.jpg"
+  }
+]);
