f817d63d342e7c2b9601e371e40400c4189a0a45
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-04-17 20:04:41 -0300

    feat(Component: EmptyAreaMessage): :sparkles: Add a menssage to indicate empty content in blog page

--- src/app/(main-pages)/blog/page.tsx
+++ src/app/(main-pages)/blog/page.tsx
@@ -1,16 +1,21 @@
+import { EmptyAreaMessage } from "@/components/EmptyAreaMessage";
 import { PostCard } from "@/components/PostCard";
 import { prismicClient } from "@/prismicio";
+import { AllDocumentTypes } from "../../../../prismicio-types";
 
 export default async function BlogPage() {
 	const client = prismicClient();
 
-	const posts = await client.getAllByType("blog_post_default");
+	const posts: AllDocumentTypes[] = await client.getAllByType("blog_post_default");
 
 	return (
 		<div className="flex h-full flex-col justify-between gap-5 text-ctp-text">
-			<header>
-				<p className="text-right text-sm">{posts.length} postagem(s)</p>
-			</header>
+			{!!posts.length && (
+				<header>
+					<p className="text-right text-sm">{posts.length} postagem(s)</p>
+				</header>
+			)}
+			{posts.length === 0 && <EmptyAreaMessage className="h-full" />}
 			<main className="flex flex-col gap-5">
 				{posts.map(post => {
 					return <PostCard key={post.uid} post={post} />;
