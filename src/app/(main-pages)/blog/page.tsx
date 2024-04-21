1267d7df8c432515203f612c7781cca26a011c73
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-04-20 22:35:46 -0300

    chore: :mute: Remove unecessary console.log

--- src/app/(main-pages)/blog/page.tsx
+++ src/app/(main-pages)/blog/page.tsx
@@ -38,7 +38,6 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
 
 	const filteredPosts = posts.filter(post => {
 		const filter = searchParams?.filter?.toLowerCase();
-		console.log(filter);
 
 		if (!filter) return true;
 
